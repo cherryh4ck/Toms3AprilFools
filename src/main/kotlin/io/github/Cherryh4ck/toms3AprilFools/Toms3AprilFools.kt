0ddd4f021157bf92d6f725c84b9e958b357c3f39
Cherryh4ck <tomas1234567891016@gmail.com>
2026-03-26 23:42:36 -0300

    Initial commit

---
+++ src/main/kotlin/io/github/Cherryh4ck/toms3AprilFools/Toms3AprilFools.kt
@@ -0,0 +1,14 @@
+package io.github.Cherryh4ck.toms3AprilFools
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Toms3AprilFools : JavaPlugin() {
+
+    override fun onEnable() {
+        // Para empezar luego :3
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
